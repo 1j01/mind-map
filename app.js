5d3bfe7bf3fcb0f7e8a63ec09c589274edc940fe
Isaiah Odhner <isaiahodhner@gmail.com>
2015-01-26 00:35:44 -0500

    firebase? (committing old code)
    i probably shouldn't be just committing this
    dunno what the result may be
    i think it still says it saved 'only locally', 'so watch out' which is
    inaccurate

--- app.js
+++ app.js
@@ -3,7 +3,45 @@ $(function(){
 	var top_level_nodes = [];
 	
 	var doc_name = location.search || "document";
-	load();
+	
+	var fb = new Firebase("https://mind-map.firebaseio.com/");
+	fb.child("stats/v").transaction(function(val){
+		return (val||0) + 1;
+	});
+	var fb_doc = fb.child("documents").child(doc_name);
+	var fb_nodes = fb_doc.child("nodes");
+	/*fb_doc.once("value", function(snapshot){
+		var doc = snapshot.val();
+		function load_node(node, i){
+			$Node(node, fb_nodes.child(i)).content(node._);
+			//if(node.c){
+			//	$.each(node.c, load_node);
+			//}
+		}
+		if(doc && doc.nodes){
+			$.map(doc.nodes, load_node);
+		}
+		if($(".n").length === 0){
+			$Node({
+				x: innerWidth / 2,
+				y: innerHeight / 3,
+			}).focus();
+		}
+	});*/
+	fb_nodes.on('child_added', function(snapshot){
+		var already_got_that_one = false;
+		$.each(top_level_nodes, function(i, node){
+			if(node.fb.name() === snapshot.ref().name()){
+				already_got_that_one = true;
+			}
+		});
+		if(!already_got_that_one){
+			$Node(snapshot.val(), snapshot.ref());
+		}
+	});
+	
+	
+	//load();
 	
 	function load(){
 		function load_node(node){
@@ -59,16 +97,27 @@ $(function(){
 	});
 	
 	var $last;
-	function cleanup(){
+	function $Node(o, fb_n){
+		fb_n = fb_n || fb_nodes.push(o);
+		
 		if($last && $last.isEmpty()){
 			var idx = top_level_nodes.indexOf($last);
 			top_level_nodes.splice(idx, 1);
 			$last.remove();
 		}
-		$last = null;
-	}
-	function $Node(o){
-		cleanup();
+		function cleanup(){
+			if($n.isEmpty()){
+				if($last && $last !== $n){
+					if($last && $last.isEmpty()){
+						var idx = top_level_nodes.indexOf($last);
+						top_level_nodes.splice(idx, 1);
+						$last.remove();
+					}
+					$last = null;
+				}
+			}
+		}
+		
 		var previous_content = "";
 		var $n = $last = $("<div contenteditable class='n'></div>")
 		.appendTo("body")
@@ -79,12 +128,8 @@ $(function(){
 			fontSize: "2em"
 		})
 		.on("keydown", function(e){
-			if($n.isEmpty()){
-				if($last && $last !== $n){
-					cleanup();
-				}
-				$last = $n;
-			}
+			cleanup();
+			$last = $n;
 			if(e.keyCode === 13 && !e.shiftKey){
 				e.preventDefault();
 				$Node({
@@ -100,17 +145,25 @@ $(function(){
 			var content = $n.content();
 			if(previous_content !== content){
 				save();
+				if(content){
+					fb_n.set({x:$n.x, y:$n.y, _:content});
+				}else{
+					fb_n.remove();
+				}
 			}
 			previous_content = content;
 		})
 		.on("mousedown", function(e){
 			e.stopPropagation();
+			cleanup();
+			$last = $n;
 		});
 		
+		$n.fb = fb_n;
 		$n.content = function(html){
-			if(html){
+			if(typeof html === "string"){
 				previous_content = html;
-				$n.html(html);
+				$n.html() !== html && $n.html(html);
 				position();
 				setTimeout(position);
 				setTimeout(position);
@@ -125,12 +178,36 @@ $(function(){
 			}
 			return $n.text().match(/^\s*$/);
 		};
+		$n.rem = function(){
+			$n.remove();
+			fb_n.remove();
+		};
 		
 		$n.x = o.x;
 		$n.y = o.y;
 		position(true);
 		
 		top_level_nodes.push($n);
+		
+		fb_n.once('value', function(snapshot){
+			var v = snapshot.val();
+			if(!v){
+				fb_n.set(o);
+			}
+		});
+		fb_n.on('value', function(snapshot){
+			var v = snapshot.val();
+			if(v){
+				$n.x = v.x;
+				$n.y = v.y;
+				$n.content(v._);
+				position(true);
+				$n.show();
+			}else{
+				//if($n !== $last)
+				$n.hide();
+			}
+		});
 		
 		return $n;
 		
